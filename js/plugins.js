d6217c4ce9a091e9e295d85ae0a1bb49a17b5e20
Jonnhy98 <38073621+Jonnhy98@users.noreply.github.com>
2019-01-04 12:12:22 +0100

    initial commit

--- js/plugins.js
+++ js/plugins.js
@@ -4,5 +4,5 @@ var $plugins =
 [
 {"name":"Community_Basic","status":true,"description":"Plugin used to set basic parameters.","parameters":{"cacheLimit":"20","screenWidth":"816","screenHeight":"624","changeWindowWidthTo":"","changeWindowHeightTo":"","renderingMode":"auto","alwaysDash":"off"}},
 {"name":"MadeWithMv","status":false,"description":"Show a Splash Screen \"Made with MV\" and/or a Custom Splash Screen before going to main screen.","parameters":{"Show Made With MV":"true","Made with MV Image":"MadeWithMv","Show Custom Splash":"false","Custom Image":"","Fade Out Time":"120","Fade In Time":"120","Wait Time":"160"}},
-{"name":"ScreenResolution","status":true,"description":"v1.00 Change your game's screen resolution to your liking!","parameters":{"Screen Width":"1104","Screen Height":"624"}}
+{"name":"ScreenResolution","status":false,"description":"v1.00 Change your game's screen resolution to your liking!","parameters":{"Screen Width":"1104","Screen Height":"624"}}
 ];
