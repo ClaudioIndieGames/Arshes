ccf0c9eefd4c75cd56354df8cb45fd3f90f72627
Jonnhy98 <38073621+Jonnhy98@users.noreply.github.com>
2019-01-04 22:54:50 +0100

    initial commit

--- www/js/plugins/FullScreen_Web.js
+++ www/js/plugins/FullScreen_Web.js
@@ -35,33 +35,6 @@ AmyPond.minHeight = Number(AmyPond.parameters['Minimum Height'] || 0);
 AmyPond.maxWidth = Number(AmyPond.parameters['Maximum Width'] || 0);
 AmyPond.maxHeight = Number(AmyPond.parameters['Maximum Height'] || 0);
 
-var elem = document.AmyPond;
-
-/* View in fullscreen */
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.mozRequestFullScreen) { /* Firefox */
-    elem.mozRequestFullScreen();
-  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE/Edge */
-    elem.msRequestFullscreen();
-  }
-}
-
-/* Close fullscreen */
-function closeFullscreen() {
-  if (document.exitFullscreen) {
-    document.exitFullscreen();
-  } else if (document.mozCancelFullScreen) { /* Firefox */
-    document.mozCancelFullScreen();
-  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
-    document.webkitExitFullscreen();
-  } else if (document.msExitFullscreen) { /* IE/Edge */
-    document.msExitFullscreen();
-  }
-}
 
 AmyPond.resize = function() {
 
