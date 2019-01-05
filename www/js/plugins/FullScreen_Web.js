dedf8d0f18b15d9caafce38121bd02605d91bce0
Jonnhy98 <38073621+Jonnhy98@users.noreply.github.com>
2019-01-05 20:22:17 +0100

    initial commit

--- www/js/plugins/FullScreen_Web.js
+++ www/js/plugins/FullScreen_Web.js
@@ -34,7 +34,19 @@ AmyPond.minWidth = Number(AmyPond.parameters['Minimum Width'] || 0);
 AmyPond.minHeight = Number(AmyPond.parameters['Minimum Height'] || 0);
 AmyPond.maxWidth = Number(AmyPond.parameters['Maximum Width'] || 0);
 AmyPond.maxHeight = Number(AmyPond.parameters['Maximum Height'] || 0);
-
+var elem = document.documentElement;
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+} 
+openFullscreen();
 
 AmyPond.resize = function() {
     AmyPond.w = window.screen.width * window.devicePixelRatio;  //window.innerWidth
@@ -72,21 +84,8 @@ AmyPond.resize = function() {
     SceneManager._boxHeight = Math.round((AmyPond.h * 816)/AmyPond.w);
     window.resizeBy(AmyPond.w,AmyPond.h);
 };
-var elem = document.documentElement;
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.mozRequestFullScreen) { /* Firefox */
-    elem.mozRequestFullScreen();
-  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE/Edge */
-    elem.msRequestFullscreen();
-  }
-} 
 
 AmyPond.resize();
-openFullscreen();
 //=============================================================================
 // End of File
 //=============================================================================
